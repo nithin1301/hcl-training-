d61ca5beee3c3f745ee50d1e391d3fde689493ca
nithin1301 <user.email>
2021-04-19 12:19:20 +0530

    login validation

---
+++ login validation/script.js
@@ -0,0 +1,9 @@
+function valid(){
+    var one=document.getElementById("username").value
+    var two=document.getElementById("password").value
+    if (one == ""||two == "")
+    {
+        alert("please fill the feild")
+    
+    }
+}
